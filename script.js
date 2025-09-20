adc3d94fefa437993f28f6c673c8e7b917d875f9
Yashaswi2907 <yashaswis545@gmail.com>
2025-09-20 22:48:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+let uls = document.querySelector("ul");
+uls.addEventListener("click", function(dets){
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+})
